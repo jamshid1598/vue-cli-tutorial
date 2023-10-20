323418237c10ac4f5a31af044b22491387bf6469
jamshid1598 <dovurovjamshid982@gmail.com>
2023-10-20 15:01:59 +0500

    lesson5 - creating another modal

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,74 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="modal1" v-if="show_modal">
+    <Modal status="" @close="toggleModal">
+      <h1>{{ modal_header }}</h1>
+      <p>{{ modal_p_text }}</p>
+      <template v-slot:links>
+        <a href="#">sign up</a>
+        <a href="#">sign in</a>
+      </template>
+    </Modal>
+  </div>
+
+  <div class="modal2" v-if="show_info_modal">
+    <Modal @close="toggleInfoModal">
+      <h1>{{ info_modal_heading }}</h1>
+      <p>{{ info_modal_text }}</p>
+      <template v-slot:links>
+        <a href="#">view more</a>
+        <a href="#">next task</a>
+      </template>
+    </Modal>
+  </div>
+
+  <h1>{{ title }}</h1>
+
+  <br>
+
+  <input type="text" ref="input_name">
+  <button @click="clickHandler">click me</button>
+  
+  <p>click to see modal</p>
+  <button @click="toggleModal">show modal</button>
+  
+  <p>click to see info modal</p>
+  <button @click="toggleInfoModal">show info modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal
+  },
+  data() {
+    return {
+      title: "This is the first title of my Vue app",
+      modal_header: "modal heading from props",
+      modal_p_text: "this is small paragraph text from props",
+      show_modal: false,
+
+      info_modal_heading : "this is secondary heading",
+      info_modal_text: "some random text",
+      show_info_modal: false,
+    }
+  },
+  methods: {
+    clickHandler() {
+      console.log(this.$refs.input_name)
+      this.$refs.input_name.classList.add("FISH")
+      this.$refs.input_name.focus()
+    },
+
+    toggleModal() {
+      this.show_modal = !this.show_modal
+    },
+
+    toggleInfoModal() {
+      this.show_info_modal = !this.show_info_modal
+    },
   }
 }
 </script>
@@ -22,5 +81,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid cadetblue;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+
+input {
+  margin-top: 50px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{ done: status === 'done' }">
+            <slot>modal heading and some text goes here</slot> <!-- text insight slot is default values -->
+
+            <div class="auth_links">
+                <slot name="links">auth links goes here</slot> <!-- text insight slot is default values -->
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    props: [
+        'status',
+    ],
+    methods: {
+        closeModal() {
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
+
+<style> /* `scoped` keyword is used for defining this style appleis only for elements of templates in this component  */
+.modal {
+    width: 400px;
+    padding: 20px;
+    margin: 200px auto;
+    background: white;
+    border-radius: 10px;
+}
+
+.backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+}
+
+.modal h1 { /* `.modal h1 {}` using selector to define exact html element  */
+    color: #03cfb4;
+    border: none;
+    padding: 0;
+}
+
+.modal .auth_links {
+    text-align: center;
+    margin: 30px 0 10px 0;
+}
+
+.auth_links a {
+    color: #333;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+}
+
+.modal.done {
+    background: crimson;
+    color: white;
+}
+
+.modal.done h1 {
+    color: white;
+}
+
+.modal.done .auth_links {
+    color: white;
+}
+
+.modal.done .auth_links a {
+    color: white;
+}
+
+</style>
